38bafbce0301fccce7a0232f1169e4e6e84282cd
ESET0103 <mantu.kumar@esyasoft.com>
2025-09-03 13:21:56 +0530

    functional changes

--- Embedded_System/ReadMe.md
+++ Embedded_System/ReadMe.md
@@ -1,7 +1,7 @@
 Introduction to the basic Embedded C Programming : <br>
 File Names : <br>
     1. ledBlink.c - 
-        Contains basic programming to blink an led light for maxm 5 times. It has blink threshold of 3 times. For this a counter is maintained. When led is on , there is delay of t sec and when it off there is delay of t second for the  second blink and so on. But if the blinks/counter is more than threshold delays increase by k times means delay will of (t + k) seconds;
+        Contains basic programming to blink an led light for maxm 5 times. It has blink threshold of 3 times. For this a counter is maintained. When led is on , there is delay of t sec and when it off there is delay of t second for the  second blink and so on. But if the blinks/counter is more than threshold delays increase by k times means delay will of (t + k) seconds;<br>
 
     2. ledBlink_Button_input_control.c - 
         Contains programming code to blink an led light from a button. if (let say button/switch is 'A' or 'a') led it off and pressing
--- Embedded_System/ledBlink_Button_input_control.c
+++ Embedded_System/ledBlink_Button_input_control.c
@@ -1,35 +1,34 @@
 #include<stdio.h>
 #include<stdbool.h>
 
+// bool flag = false;      // global variable for checking if led is ON or OFF;
 
 // function is turning the LED ON or OFF
-void on_off(bool flag){
-    if(!flag){
-        flag = true;        // if flag is true, led glows;
+void on_off(bool *flag){
+    if(*flag == false){
+        *flag = true;        // if flag is true, led glows;
         printf("Led is Turn ON \n");
     }
     else{
-        flag = false;       // if flag is flase, led turns off;
+        *flag = false;       // if flag is flase, led turns off;
         printf("Led is turn OFF \n");
     }
 }
 
-bool flag = false;      // global variable for checking if led is ON or OFF;
 
 
 // main function ....
 int main(){
+    bool flag = false;      // global variable for checking if led is ON or OFF;
 
     while(true){
         char button;
         printf("Press the button: ");
         scanf(" %c",&button);
-        if((button == 'a' || button == 'A') && flag == false ){         // if pressed button is 'a' or 'A' and flag is false , then led glows;
-            on_off(flag);
+        if((button == 'a' || button == 'A') ){         // if pressed button is 'a' or 'A' to turn led ON or OFF ;
+            on_off(&flag);
         }
-        else if((button == 'a' || button == 'A') && flag == true){      // if pressed button is 'a' or 'A' and flag is true , then led is off;
-           on_off(flag);
-        }
+       
         else{                                                           // if any other button is pressed , then the loop should terminate
             break;
         }
