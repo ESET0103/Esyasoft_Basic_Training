7bae41e368ad8ad7f1d6cc240c65d74e3e2873eb
ESET0103 <mantu.kumar@esyasoft.com>
2025-09-17 17:46:01 +0530

    get-request-code

---
+++ Basics_of_DLMS/DLMS_get_request/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DLMS_get_request
+{
+    
+    internal class Program
+    {
+        // GET Request APDU builder
+        
+
+        static void Main(string[] args)
+        {
+            var obis = new ObisCode(1, 0, 1, 8, 0, 255);
+
+            // Class ID for Register object is 3
+            ushort classId = 3;
+            byte attributeId = 2; // e.g., value attribute
+
+            byte[] apdu = GetRequestApdu.CreateGetRequest(obis, classId, attributeId);
+
+            Console.WriteLine("OBIS Code: " + obis);
+            Console.WriteLine("GET Request APDU: " + BitConverter.ToString(apdu));
+        }
+    }
+}
