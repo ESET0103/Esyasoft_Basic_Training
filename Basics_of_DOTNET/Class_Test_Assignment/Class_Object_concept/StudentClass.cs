054dadf87dc4e288c5d477728e048b157df42b8e
ESET0103 <mantu.kumar@esyasoft.com>
2025-09-18 13:03:24 +0530

    class - object concept

---
+++ Basics_of_DOTNET/Class_Test_Assignment/Class_Object_concept/StudentClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    internal class Student
+    {
+        int student_id;
+        string name;
+        int age;
+        string contact_no;
+        string email;
+
+        public Student(int student_id, string name, int age, string contact_no, string email)
+        {
+            this.student_id = student_id;
+            this.name = name;
+            this.age = age;
+            this.contact_no = contact_no;
+            this.email = email;
+        }
+
+        //public void set(int id, string name, int age ,string contact_no, string email)
+        //{
+        //    this.student_id = id;
+        //    this.name = name;   
+        //    this.age = age; 
+        //    this.contact_no = contact_no;
+        //    this.email = email;
+
+        //}
+
+        public int total_marks() { return 0; }
+        public int resultStatus(int status) { return 0; }
+
+
+        public void showDetails()
+        {
+            Console.WriteLine($"Student Id : {student_id}");
+            Console.WriteLine($"Name : {name}");
+            Console.WriteLine($"Age : {age}");
+            Console.WriteLine($"Email : {email}");
+            Console.WriteLine($"Contact Number : {contact_no}");
+            //Console.WriteLine("Total Marks: ");
+        }
+    }
+}
---
+++ Basics_of_DOTNET/Class_Test_Assignment/Class_Object_concept/StudentProgram.cs
@@ -0,0 +1,13 @@
+﻿namespace ClassDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student(1, "Sahil khan", 26, "987654315", "sahilkhan@gmail.com");
+            //student.set(1, "Sahil khan",26,"987654315","sahilkhan@gmail.com");
+
+            student.showDetails();
+        }
+    }
+}
