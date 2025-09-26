008aff87c1673b2281a798c33124d117efaf89e0
ESET0103 <mantu.kumar@esyasoft.com>
2025-09-26 22:39:18 +0530

    Strech-goal - Webapi to connect database and fetch data

---
+++ Basics_of_DOTNET/Strech_Goal/Web_Api_Demo/Models/Products.cs
@@ -0,0 +1,9 @@
+﻿namespace Web_Api_Demo.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Names { get; set; }
+        public int Price { get; set; }
+    }
+}
